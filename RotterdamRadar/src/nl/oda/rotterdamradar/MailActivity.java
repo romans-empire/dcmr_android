aa66a815b9dc9c2cffb157f08f25b4dfcab9a270
Richard <richard-lindhout@hotmail.com>
2013-06-05 19:45:42 +0200

    contactformulier

--- RotterdamRadar/src/nl/oda/rotterdamradar/MailActivity.java
+++ RotterdamRadar/src/nl/oda/rotterdamradar/MailActivity.java
@@ -19,8 +19,8 @@ import android.widget.Toast;
 public class MailActivity extends Activity implements OnClickListener,
 		OnItemSelectedListener {
 	/** Called when the activity is first created. */
-	EditText naamField, mailField, mobielField, berichtField, postcodeField;
-	Spinner subject;
+	EditText straatnaamField, woonplaatsField, subtoelichtingField, terugkoppelingField, naamField, mailField, mobielField, toelichtingField, postcodeField;
+	Spinner subject, spinner, spinner1, spinner2;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -30,16 +30,48 @@ public class MailActivity extends Activity implements OnClickListener,
 		naamField = (EditText) findViewById(R.id.editTekstNaam);
 		mailField = (EditText) findViewById(R.id.editTextMail);
 		mobielField = (EditText) findViewById(R.id.editTextMobiel);
-		berichtField = (EditText) findViewById(R.id.editTextBericht);
+		toelichtingField = (EditText) findViewById(R.id.toelichting);
 		postcodeField = (EditText) findViewById(R.id.editPostcode);
+		straatnaamField = (EditText) findViewById(R.id.editStraatnaam);
+		woonplaatsField = (EditText) findViewById(R.id.editWoonplaats);
+		//subtoelichtingField = (EditText) findViewById(R.id.subtoelichting);
+		//terugkoppelingField = (EditText) findViewById(R.id.terugkoppeling);
 
 		subject = (Spinner) findViewById(R.id.spinner);
-		String subjects[] = new String[] { "Vul in", "Stankoverlast", "Geluidsoverlast", "Stofoverlast", "Lichtoverlast" };
+		String subjects[] = new String[] { "Hoofd Overlast", "afval", "bodem", "Stankoverlast", "Geluidsoverlast", "Stofoverlast", "Lichtoverlast" };
 		subject.setOnItemSelectedListener(this);
 		ArrayAdapter<String> sa = new ArrayAdapter<String>(this,
 				R.layout.spinner_item, subjects);
 		sa.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
 		subject.setAdapter(sa);
+		;
+		
+
+		spinner = (Spinner) findViewById(R.id.spinner1);
+		String onderwerpen[] = new String[] {"Extra Overlast", "riool", "kassen", "rook", "vliegtuig", "fabriek", "muziek" };
+		spinner.setOnItemSelectedListener(this);
+		ArrayAdapter<String> subaard = new ArrayAdapter<String>(this,
+				R.layout.spinner_item, onderwerpen);
+		subaard.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+		spinner.setAdapter(subaard);
+		;
+		
+		spinner1 = (Spinner) findViewById(R.id.spinner2);
+		String subonderwerpen[] = new String[] {"Extra extra Overlast", "helicopter", "brandlucht", "DJ", "feest" };
+		spinner1.setOnItemSelectedListener(this);
+		ArrayAdapter<String> subsubaard = new ArrayAdapter<String>(this,
+				R.layout.spinner_item, subonderwerpen);
+		subsubaard.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+		spinner1.setAdapter(subsubaard);
+		;
+		
+		spinner2 = (Spinner) findViewById(R.id.spinner3);
+		String terugkoppeling[] = new String[] {"kies", "ja", "nee", };
+		spinner2.setOnItemSelectedListener(this);
+		ArrayAdapter<String> terugkoppeling1 = new ArrayAdapter<String>(this,
+				R.layout.spinner_item, terugkoppeling);
+		terugkoppeling1.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+		spinner2.setAdapter(terugkoppeling1);
 		;
 
 		final Button buttonSend = (Button) findViewById(R.id.buttonSend);
@@ -51,25 +83,37 @@ public class MailActivity extends Activity implements OnClickListener,
 			naamField.setError("Vul uw naam in");
 		} else if (mailField.getText().toString().length() == 0) {
 			mailField.setError("Vul uw email in");
+		} else if (straatnaamField.getText().toString().length() == 0){
+			straatnaamField.setError("Vul straatnaam in");
+		} else if (woonplaatsField.getText().toString().length() == 0){
+			woonplaatsField.setError("Vul woonplaats in");
 		} else if (mobielField.getText().toString().length() != 10) {
 			mobielField.setError("Vul een geldig telefoonnummer in");
 		} else if (postcodeField.getText().toString().length() == 0) {
 			postcodeField.setError("Vul een postcode in");
-		} else if (berichtField.getText().toString().length() == 0) {
-			berichtField.setError("Vul uw bericht in");
 		} else if (subject.getSelectedItemPosition() == 0) {
-			Toast.makeText(MailActivity.this, "Please select the Subject",
+			Toast.makeText(MailActivity.this, "Vul het onderwerp in",
 					Toast.LENGTH_SHORT).show();
+		} else if (spinner1.getSelectedItemPosition() == 0) {
+			Toast.makeText(MailActivity.this, "Vul terugkoppeling in",
+					Toast.LENGTH_SHORT).show();
+		
 		} else {
 			String body = "Name : " + naamField.getText().toString()
 					+ "<br>Mobile :" + mobielField.getText().toString()
 					+ "<br>Email :" + mailField.getText().toString()
-					+ "<br>Bericht :" + berichtField.getText().toString()
-					+ "<br>Postcode :" + postcodeField.getText().toString();
+					+ "<br>Eventuele opmerkingen :" + toelichtingField.getText().toString()
+					+ "<br>Postcode :" + postcodeField.getText().toString()
+					+ "<br>Straatnaam :" + straatnaamField.getText().toString()
+					+ "<br>Woonplaats :" +  woonplaatsField.getText().toString()
+					+ "<br>Terugkoppeling :" + spinner2.getSelectedItem().toString()
+					+ "<br>onderwerp :" + subject.getSelectedItem().toString()
+					+ "<br>Extra onderwerp  :" + spinner.getSelectedItem().toString()
+					+ "<br>Nog meer extra's :" + spinner1.getSelectedItem().toString();
 
 			Intent email = new Intent(Intent.ACTION_SEND);
 			email.putExtra(Intent.EXTRA_EMAIL, new String[] { "richard-lindhout@hotmail.com" });
-			email.putExtra(Intent.EXTRA_SUBJECT, subject.getSelectedItem().toString());
+			email.putExtra(Intent.EXTRA_SUBJECT, new String[] { "klacht" });
 			email.putExtra(Intent.EXTRA_TEXT, Html.fromHtml(body));
 			email.setType("message/rfc822");
 			startActivityForResult(Intent.createChooser(email, "Richard"), 1);
@@ -80,7 +124,7 @@ public class MailActivity extends Activity implements OnClickListener,
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
 		new AlertDialog.Builder(MailActivity.this)
-				.setMessage("Your requested has been Accepted\nThank You")
+				.setMessage("Uw bericht is verstuurd!\nDank u.")
 				.setCancelable(false)
 				.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
 					public void onClick(DialogInterface dialog, int which) {
